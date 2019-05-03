096c78ce3a8123eac93933e97666b48bf099f184
Raphael Lopes <ph.campanario07@gmail.com>
2019-05-03 16:25:39 -0300

    Configure base map

--- src/components/map/index.jsx
+++ src/components/map/index.jsx
@@ -1,6 +1,72 @@
 // vendor
-import React from 'react';
+import React, { Component } from 'react';
+import MapGl, { Marker } from 'react-map-gl';
+import 'mapbox-gl/dist/mapbox-gl.css';
 
-const MapBox = () => <h1>My Map Page</h1>;
+// locals
+import mapboxConfig from '../../config/mapbox';
 
-export default MapBox;
+export default class MapBox extends Component {
+  state = {
+    viewport: {
+      width: window.innerWidth,
+      height: window.innerHeight,
+      latitude: -23.5439948,
+      longitude: -46.6065452,
+      zoom: 14,
+    },
+  }
+
+  componentDidMount() {
+    window.addEventListener('resize', this.resize);
+    this.resize();
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.resize);
+  }
+
+  resize = () => {
+    this.setState({
+      viewport: {
+        ...this.state.viewport,
+        width: window.innerWidth,
+        height: window.innerHeight,
+      },
+    });
+  }
+
+  handleMapClick(e) {
+    const [latitude, longitude] = e.lngLat;
+    alert(`Lat: ${latitude} \nLong: ${longitude}`);
+  }
+
+  render() {
+    return (
+      <MapGl
+        {...this.state.viewport}
+        onClick={this.handleMapClick}
+        mapStyle="mapbox://styles/mapbox/basic-v9"
+        mapboxApiAccessToken={mapboxConfig.accessToken}
+        onViewportChange={viewport => this.setState({ viewport })}
+      >
+        <Marker
+          latitude={-23.5439948}
+          longitude={-46.6065452}
+          onClick={this.handleMapClick}
+          captureClick
+        >
+          <img
+            style={{
+              borderRadius: 100,
+              width: 48,
+              height: 48,
+            }}
+            src="https://avatars2.githubusercontent.com/u/2254731?v=4"
+            alt=""
+          />
+        </Marker>
+      </MapGl>
+    );
+  }
+}
---
+++ src/config/mapbox.js
@@ -0,0 +1,3 @@
+const accessToken = 'pk.eyJ1IjoicmFwaGFlbGxvcGVzMDciLCJhIjoiY2p2OGU5NXVzMHB2ZzQ0cXAycncwMjFzeiJ9.JB-xOz6qKLdV80FS1t5z1A';
+
+export default { accessToken };
